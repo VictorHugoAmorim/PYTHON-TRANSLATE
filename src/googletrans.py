718249cf3329fbaae122ba4739033c8379beed38
Victor Hugo <victor.hugo.amorim.7@gmail.com>
2021-09-18 23:01:12 -0300

    update input do usuario

--- src/googletrans.py
+++ src/googletrans.py
@@ -1,7 +1,7 @@
 from translate import Translator
 
 s = Translator(from_lang="english", to_lang="Portuguese")
+texto = input("Digite o texto a ser traduzido:\n")
+res= s.translate(texto)
 
-res= s.translate("Hello World")
-
-print(res)
+print(f'Inglês: "{texto.upper()}"\nPortuguês: "{res}"')
