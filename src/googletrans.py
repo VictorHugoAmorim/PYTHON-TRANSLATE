d3c1c57b7394db03e9ccbc5a008ec3ea7d7457a7
Victor Hugo <victor.hugo.amorim.7@gmail.com>
2021-09-18 23:09:29 -0300

    Tradutor por voz
    Captura audio e converte em inglês

--- src/googletrans.py
+++ src/googletrans.py
@@ -1,7 +1,67 @@
+import speech_recognition as sr
+import pyaudio
+import time
+#from googletrans import Translator
 from translate import Translator
 
-s = Translator(from_lang="english", to_lang="Portuguese")
-texto = input("Digite o texto a ser traduzido:\n")
-res= s.translate(texto)
+#Função para ouvir e reconhecer a fala
+def ouvir_microfone():
+    #Habilita o microfone do usuário
+    microfone = sr.Recognizer()
+    
+    #usando o microfone
+    with sr.Microphone() as source:
+        
+        #Chama um algoritmo de reducao de ruidos no som
+        microfone.adjust_for_ambient_noise(source)
+        
+        #Frase para o usuario dizer algo
+        print("Aguardando...")
+        
+        #Armazena o que foi dito numa variavel
+        audio = microfone.listen(source)
+        
+    try:
+        
+        #Passa a variável para o algoritmo reconhecedor de padroes
+        frase = microfone.recognize_google(audio,language='pt-BR')
+        
+        #Retorna a frase pronunciada
+        print("Você disse: " + frase)
+        
+    #Se nao reconheceu o padrao de fala, exibe a mensagem
+    except sr.UnknownValueError:
+        print("Não entendi")
 
-print(f'Inglês: "{texto.upper()}"\nPortuguês: "{res}"')
+        
+    return frase
+
+
+print('Ola, sou sua Tradutora Virtual!\nDiga "OK" para iniciarmos')
+
+frase=ouvir_microfone()
+start = 'ok'
+
+while start.upper() != frase.upper():
+    print('comando não reconhecido, favor repetir')
+    frase=ouvir_microfone()
+if start.upper() == frase.upper():
+    pass
+    while True:
+        #Gerando tradução
+        s = Translator(from_lang="Portuguese", to_lang="English")
+        frase=ouvir_microfone()
+        res= s.translate(frase)
+        '''
+        #Gerando tradução
+        trad = Translator()
+        frase=ouvir_microfone()
+        res = trad.translate(str(frase), dest="en").text'''
+        print(f'Português: "{frase.upper()}"\nInglês: "{res}"')
+        if "Encerrar".upper() in frase.upper():
+            print('Até logo')
+            break
+
+
+    
+
